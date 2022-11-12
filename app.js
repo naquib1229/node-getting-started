4e0f37b02ee3e366f1054928e003b0a9ff5f6ebf
naquib1229 <naquib.alam1229@gmail.com>
2022-11-12 16:06:50 +0530

    HTTP Module req, res Multiple Routes

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const server = http.createServer((req, res)=> {
         res.write('Hello world');
         res.end();
     }
+
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 }); 
 
 
