c10d23380716ac3eca70ccb1ddd62a41fb7a2f5d
naquib1229 <naquib.alam1229@gmail.com>
2022-11-12 15:59:41 +0530

    HTTP Module req, res

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 const http = require('http');
 
-const server = http.createServer(); //server is an event emitter.
+const server = http.createServer((req, res)=> {
+    if(req.url ==='/'){
+        res.write('Hello world');
+        res.end();
+    }
+}); 
 
-server.on('connection',(socket) => {
-    console.log('New connection ...')
-})
+
 server.listen(3000); //port 3000
 
 console.log('Listening on port 3000...');
