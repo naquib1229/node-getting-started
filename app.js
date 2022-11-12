e7df254f1a492d8c55c84b2213240214a6c94709
naquib1229 <naquib.alam1229@gmail.com>
2022-11-12 14:20:25 +0530

    Event Modules Register a listener Raise an Event

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
+//Event- A signal that something has happened in our application. Several class in node raises different kinds of events.
+//And in your code, you might be interested to respond to those events.
+//EventEmitter is one of the class of Events. It's one of the core building blocks of Node and a lot of classes are based on this EventEmitter.
 
-// const files = fs.readdirSync('./');//not preferred use instead readdir asynchronous version
-// console.log(files)
+const EventEmitter = require('events'); //naming,EventEmitter first letter of every word is upper case indicates class 
+const emitter = new EventEmitter(); //here emitter is an object
 
-fs.readdir('./',function(err, files){
-    if(err) console.log('Error',err);
-    else console.log('Result',files);
-}); 
-    //first argument is path and the 2nd is now all these asynchronous method take a function as their last argument.
-    // Node will call this function when that asynchronous operation completes.we call this function a call back.
+//of course order is important here, if you register this listener after calling the emit method, nothing would have happened.
+//Because when we call emit method, this emitter iterates over all the registered listeners and calls them synchronously.
 
+
+//Register a listener
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
+});
+
+//Raise an event
+emitter.emit('messageLogged');
 
 
 //output
 
 /* 
- Result [ '.git', 'Node Getting started.odt', 'app.js', 'logger.js' ]
+Listener called
 */
 
