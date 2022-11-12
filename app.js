81c6c12f07eca933fdec34cccde631608606b5f0
naquib1229 <naquib.alam1229@gmail.com>
2022-11-12 13:54:08 +0530

    File system readdir callback

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
-const files = fs.readdirSync('./');//not preferred use instead readdir asynchronous version
-console.log(files)
+// const files = fs.readdirSync('./');//not preferred use instead readdir asynchronous version
+// console.log(files)
+
+fs.readdir('./',function(err, files){
+    if(err) console.log('Error',err);
+    else console.log('Result',files);
+}); 
+    //first argument is path and the 2nd is now all these asynchronous method take a function as their last argument.
+    // Node will call this function when that asynchronous operation completes.we call this function a call back.
 
 
 
 //output
 
 /* 
- ['.git', 'Node Getting started.odt', 'app.js', 'logger.js']
+ Result [ '.git', 'Node Getting started.odt', 'app.js', 'logger.js' ]
 */
 
